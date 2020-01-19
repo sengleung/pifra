5899bbccbb34d426aba801baf3ae1ac0b147bbe3
Seng Leung <sleung@tcd.ie>
2020-01-19 21:38:08 +0000

    Add alpha-conversion

---
+++ ast.go
@@ -0,0 +1,96 @@
+package main
+
+import "strconv"
+
+var boundNameIndex int
+
+func doAlphaConversion(elem Element) {
+	elemTyp := elem.Type()
+	switch elemTyp {
+	case ElemTypNil:
+	case ElemTypOutput:
+		doAlphaConversion(elem.(*ElemOutput).Next)
+	case ElemTypInput:
+		doAlphaConversion(elem.(*ElemInput).Next)
+	case ElemTypMatch:
+		doAlphaConversion(elem.(*ElemMatch).Next)
+	case ElemTypRestriction:
+		resElem := elem.(*ElemRestriction)
+		boundName := resElem.Name
+		newName := generateBoundName()
+		resElem.Name = newName
+		substituteNames(resElem.Next, boundName, newName)
+		doAlphaConversion(resElem.Next)
+	case ElemTypSum:
+		sumElem := elem.(*ElemSum)
+		doAlphaConversion(sumElem.ProcessL)
+		doAlphaConversion(sumElem.ProcessR)
+	case ElemTypParallel:
+		parElem := elem.(*ElemParallel)
+		doAlphaConversion(parElem.ProcessL)
+		doAlphaConversion(parElem.ProcessR)
+	case ElemTypProcess:
+	case ElemTypProcessConstants:
+	}
+}
+
+func generateBoundName() string {
+	name := "bn_" + strconv.Itoa(boundNameIndex)
+	boundNameIndex = boundNameIndex + 1
+	return name
+}
+
+func substituteNames(elem Element, boundName string, newName string) {
+	elemTyp := elem.Type()
+	switch elemTyp {
+	case ElemTypNil:
+	case ElemTypOutput:
+		outElem := elem.(*ElemOutput)
+		if outElem.Name == boundName {
+			outElem.Name = newName
+		}
+		if outElem.Variable == boundName {
+			outElem.Variable = newName
+		}
+		substituteNames(outElem.Next, boundName, newName)
+	case ElemTypInput:
+		inpElem := elem.(*ElemInput)
+		if inpElem.Name == boundName {
+			inpElem.Name = newName
+		}
+		if inpElem.Variable == boundName {
+			inpElem.Variable = newName
+		}
+		substituteNames(inpElem.Next, boundName, newName)
+	case ElemTypMatch:
+		matchElem := elem.(*ElemMatch)
+		if matchElem.NameL == boundName {
+			matchElem.NameR = newName
+		}
+		if matchElem.NameL == boundName {
+			matchElem.NameR = newName
+		}
+		substituteNames(matchElem.Next, boundName, newName)
+	case ElemTypRestriction:
+		resElem := elem.(*ElemRestriction)
+		if resElem.Name != boundName {
+			substituteNames(resElem.Next, boundName, newName)
+		}
+	case ElemTypSum:
+		sumElem := elem.(*ElemSum)
+		substituteNames(sumElem.ProcessL, boundName, newName)
+		substituteNames(sumElem.ProcessR, boundName, newName)
+	case ElemTypParallel:
+		parElem := elem.(*ElemParallel)
+		substituteNames(parElem.ProcessL, boundName, newName)
+		substituteNames(parElem.ProcessR, boundName, newName)
+	case ElemTypProcess:
+	case ElemTypProcessConstants:
+		pcsElem := elem.(*ElemProcessConstants)
+		for i, param := range pcsElem.Parameters {
+			if param == boundName {
+				pcsElem.Parameters[i] = newName
+			}
+		}
+	}
+}
---
+++ ast_test.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/go-test/deep"
+)
+
+func TestAlphaConversion(t *testing.T) {
+	log = false
+	tests := map[string]struct {
+		input           []byte
+		declaredProcs   map[string]Element
+		undeclaredProcs []Element
+		procParams      map[string][]string
+		err             error
+	}{
+		"parallel_restriction": {
+			input: []byte(`
+$a.b(a).$a.(b'<a>.0 | $b.(a(b).0 | c(d).0))
+			`),
+			declaredProcs: map[string]Element{},
+			undeclaredProcs: []Element{
+				&ElemRestriction{
+					Name: "bn_0",
+					Next: &ElemInput{
+						Name:     "b",
+						Variable: "bn_0",
+						Next: &ElemRestriction{
+							Name: "bn_1",
+							Next: &ElemParallel{
+								ProcessL: &ElemOutput{
+									Name:     "b",
+									Variable: "bn_1",
+									Next:     &ElemNil{},
+								},
+								ProcessR: &ElemRestriction{
+									Name: "bn_2",
+									Next: &ElemParallel{
+										ProcessL: &ElemInput{
+											Name:     "bn_1",
+											Variable: "bn_2",
+											Next:     &ElemNil{},
+										},
+										ProcessR: &ElemInput{
+											Name:     "c",
+											Variable: "d",
+											Next:     &ElemNil{},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			procParams: map[string][]string{},
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			initParser()
+			lex := newLexer(tc.input)
+			yyParse(lex)
+			for _, elem := range declaredProcs {
+				doAlphaConversion(elem)
+			}
+			for _, elem := range undeclaredProcs {
+				doAlphaConversion(elem)
+			}
+			if err := deep.Equal(tc.declaredProcs, declaredProcs); err != nil {
+				spew.Dump(declaredProcs, undeclaredProcs, procParams)
+				t.Error(err)
+			}
+			if err := deep.Equal(tc.procParams, procParams); err != nil {
+				spew.Dump(declaredProcs, undeclaredProcs, procParams)
+				t.Error(err)
+			}
+			if err := deep.Equal(tc.undeclaredProcs, undeclaredProcs); err != nil {
+				spew.Dump(declaredProcs, undeclaredProcs, procParams)
+				t.Error(err)
+			}
+		})
+	}
+}
