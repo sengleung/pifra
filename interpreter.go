23eac3a1b62887316219793f9bd510fac8b45ec3
Seng Leung <sleung@tcd.ie>
2020-02-02 15:17:45 +0000

    Add get first inputs and outputs

--- interpreter.go
+++ interpreter.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/mohae/deepcopy"
+
 var registerSize = 10000
 
 type TransitionType int
@@ -65,6 +67,19 @@ type TransitionState struct {
 	Transitions *[]*TransitionState
 }
 
+type Direction int
+
+const (
+	Next Direction = iota
+	Left
+	Right
+)
+
+type Path struct {
+	Directions  []Direction
+	ElementType ElementType
+}
+
 func newTransitionStateRoot(process Element) *TransitionState {
 	freshNames := getAllFreshNames(process)
 	register := make(map[int]string, registerSize)
@@ -85,4 +100,66 @@ func newTransitionStateRoot(process Element) *TransitionState {
 
 func produceTransitionStates(ts *TransitionState) {
 	// TODO
+}
+
+func getFirstInpOuts(elem Element) []Path {
+	paths := []Path{}
+	curPath := []Direction{}
+
+	popPath := func() Direction {
+		var direction Direction
+		direction, curPath = curPath[len(curPath)-1], curPath[:len(curPath)-1]
+		return direction
+	}
+
+	var acc func(Element)
+	acc = func(elem Element) {
+		switch elem.Type() {
+		case ElemTypNil:
+		case ElemTypOutput:
+			paths = append(paths, Path{
+				Directions:  deepcopy.Copy(curPath).([]Direction),
+				ElementType: ElemTypOutput,
+			})
+		case ElemTypInput:
+			paths = append(paths, Path{
+				Directions:  deepcopy.Copy(curPath).([]Direction),
+				ElementType: ElemTypInput,
+			})
+		case ElemTypMatch:
+			matchElem := elem.(*ElemMatch)
+			if matchElem.NameL.Name == matchElem.NameR.Name {
+				curPath = append(curPath, Next)
+				acc(matchElem.Next)
+				popPath()
+			}
+		case ElemTypRestriction:
+			resElem := elem.(*ElemRestriction)
+			curPath = append(curPath, Next)
+			acc(resElem.Next)
+			popPath()
+		case ElemTypSum:
+			sumElem := elem.(*ElemSum)
+			curPath = append(curPath, Left)
+			acc(sumElem.ProcessL)
+			popPath()
+			curPath = append(curPath, Right)
+			acc(sumElem.ProcessR)
+			popPath()
+		case ElemTypParallel:
+			parElem := elem.(*ElemParallel)
+			curPath = append(curPath, Left)
+			acc(parElem.ProcessL)
+			popPath()
+			curPath = append(curPath, Right)
+			acc(parElem.ProcessR)
+			popPath()
+		case ElemTypProcess:
+		case ElemTypProcessConstants:
+		}
+	}
+
+	acc(elem)
+
+	return paths
 }
